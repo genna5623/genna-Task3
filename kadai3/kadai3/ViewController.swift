0400821067ab87c45bc3fc7b7f7aa0b372b2e723
akio0911 <akio0911@gmail.com>
2021-04-27 17:40:34 +0900

    feedback

--- kadai3/kadai3/ViewController.swift
+++ kadai3/kadai3/ViewController.swift
@@ -20,62 +20,28 @@ class ViewController: UIViewController {
     @IBOutlet private weak var secondNumSet: UILabel!
    
     @IBOutlet private weak var resultLabel: UILabel!
-    @IBAction func actionFirstSwitch(_ sender: AnyObject) {
 
-        let num1: Int = Int(self.firstNum.text!) ?? 0
-       let sum: Int
-            if firstSwitch.isOn {
-             sum = num1 * -1
-            firstNumSet.text = String(sum)
-            } else {
-           sum = num1 * 1
-            firstNumSet.text = String(sum)
-        }
-        
-    }
-    @IBAction func actionSecondSwitch(_ sender: AnyObject) {
-      
-        let num2: Int = Int(self.secondNum.text!) ?? 0
-        let sum2: Int
-        if secondSwitch.isOn {
-            sum2 = num2 * -1
-            secondNumSet.text = String(sum2)
-        } else {
-            sum2 = num2 * 1
-            secondNumSet.text = String(sum2)
-            }
-    }
-    
     @IBAction func resultButton(_ sender: Any) {
         let num1: Int = Int(self.firstNum.text!) ?? 0
         let num2: Int = Int(self.secondNum.text!) ?? 0
-        let sum: Int
-        let sum2: Int
-            
+
+        let signedNum1: Int
         if firstSwitch.isOn {
-             sum = num1 * -1
-            firstNumSet.text = String(sum)
-            } else {
-           sum = num1 * 1
-            firstNumSet.text = String(sum)
-        }
-    
-        if secondSwitch.isOn {
-            sum2 = num2 * -1
-            secondNumSet.text = String(sum2)
+            signedNum1 = -num1
         } else {
-            sum2 = num2 * 1
-            secondNumSet.text = String(sum2)
-            }
-    
-        let result = sum + sum2
+            signedNum1 = num1
+        }
+        firstNumSet.text = String(signedNum1)
+
+        let signedNum2: Int
+        if secondSwitch.isOn {
+            signedNum2 = -num2
+        } else {
+            signedNum2 = num2
+        }
+        secondNumSet.text = String(signedNum2)
+
+        let result = signedNum1 + signedNum2
         self.resultLabel.text = String(result)
-    
-}
-   
-            
-        
     }
-
-
-
+}
